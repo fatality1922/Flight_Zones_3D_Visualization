a61c7931bb0acf3e3a0443d897c547deee8ae710
fatality1922 <fatality1922@gmail.com>
2023-02-04 12:08:48 +0100

    add proper initial view, remove ifs

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, {useState, useEffect} from 'react';
 import {render} from 'react-dom';
 import {Map} from 'react-map-gl';
-import {DeckGL, View}  from '@deck.gl/react';
+import {DeckGL}  from '@deck.gl/react';
 import { GeoJsonLayer} from '@deck.gl/layers';
 import axios from 'axios';
 
   const INITIAL_VIEW_STATE = {
-      longitude: 15.136555006561679, 
-      latitude: 50.460385777406685,
-      zoom: 5,
+      longitude: 19.612237060860284, 
+      latitude: 52.023495199670975,
+      zoom: 6,
       maxZoom: 20,
-      pitch: 30,
+      pitch: 80,
       bearing: 0
   };
 
@@ -58,35 +58,37 @@ useEffect(() => {
         setTrueZones(full_zones)
   })
   }, []);
-  
 
   const colorify = (trueZones) => {
     trueZones.features.forEach(zone => {
-      if(zone.zone_type === "TSA") zone.properties.color = [255,0,42, 120]
-      if(zone.zone_type === "MRT") zone.properties.color = [15,79,0, 120]
-      if(zone.zone_type === "DRAP") zone.properties.color = [184, 6, 32, 0, 120]
-      if(zone.zone_type === "D") zone.properties.color = [191, 22, 22, 120]
-      if(zone.zone_type === "R") zone.properties.color = [255, 0, 42, 120]
-      if(zone.zone_type === "P") zone.properties.color = [245, 121, 5, 120]
-      if(zone.zone_type === "DRAR") zone.properties.color = [245, 39, 69, 120]
-      if(zone.zone_type === "DRAI") zone.properties.color = [65, 167, 250, 120]
-      if(zone.zone_type === "TRA") zone.properties.color = [199, 68, 165, 120]
-      if(zone.zone_type === "CTR1KM") zone.properties.color = [255, 0, 0, 120]
-      if(zone.zone_type === "RPA") zone.properties.color = [254, 233, 0, 0] //transparent
-      if(zone.zone_type === "CTR6KM") zone.properties.color = [178, 92, 204, 120]
-      if(zone.zone_type === "CTR") zone.properties.color = [15, 26, 217, 120]
-      if(zone.zone_type === "MCTR2KM") zone.properties.color = [255, 0, 0, 120]
-      if(zone.zone_type === "MCTR") zone.properties.color = [15, 79, 0, 120]
-      if(zone.zone_type === "ATZ1KM") zone.properties.color = [255, 0, 0, 120]
-      if(zone.zone_type === "ATZ6KM") zone.properties.color = [178, 92, 204, 120]
-      if(zone.zone_type === "ATZ") zone.properties.color = [51, 35, 21, 120]
-      if(zone.zone_type === "NW") zone.properties.color = [65, 167, 250, 120]
-      if(zone.zone_type === "AREA") zone.properties.color = [5, 114, 125, 120]
-      if(zone.zone_type === "RMZ") zone.properties.color = [201, 171, 252, 120]
-      if(zone.zone_type === "ADIZ") zone.properties.color = [128, 128, 105, 120]
-      if(zone.zone_type === "TFR") zone.properties.color = [255, 0, 42, 120]
-      if(zone.zone_type === "TMA") zone.properties.color = [59, 136, 255, 120]
-      if(zone.zone_type === "MTMA") zone.properties.color = [110, 199, 2, 120]
+      switch(zone.zone_type){
+        case 'TSA': zone.properties.color = [255,0,42, 120] ;break;
+        case "MRT" : zone.properties.color = [15,79,0, 120];break;
+        case "DRAP" : zone.properties.color = [184, 6, 32, 0, 120];break;
+        case "D" : zone.properties.color = [191, 22, 22, 120];break;
+        case "R" : zone.properties.color = [255, 0, 42, 120];break;
+        case "P" : zone.properties.color = [245, 121, 5, 120];break;
+        case "DRAR" : zone.properties.color = [245, 39, 69, 120];break;
+        case "DRAI" : zone.properties.color = [65, 167, 250, 120];break;
+        case "TRA" : zone.properties.color = [199, 68, 165, 120];break;
+        case "CTR1KM" : zone.properties.color = [255, 0, 0, 120];break;
+        case "RPA" : zone.properties.color = [254, 133, 0, 120] ;break;
+        case "CTR6KM" : zone.properties.color = [178, 92, 204, 120];break;
+        case "CTR" : zone.properties.color = [15, 26, 217, 120];break;
+        case "MCTR2KM" : zone.properties.color = [255, 0, 0, 120];break;
+        case "MCTR" : zone.properties.color = [15, 79, 0, 120];break;
+        case "ATZ1KM" : zone.properties.color = [255, 0, 0, 120];break;
+        case "ATZ6KM" : zone.properties.color = [178, 92, 204, 120];break;
+        case "ATZ" : zone.properties.color = [51, 35, 21, 120];break;
+        case "NW" : zone.properties.color = [65, 167, 250, 120];break;
+        case "AREA" : zone.properties.color = [5, 114, 125, 120];break;
+        case "RMZ" : zone.properties.color = [201, 171, 252, 120];break;
+        case "ADIZ" : zone.properties.color = [128, 128, 105, 120];break;
+        case "TFR" : zone.properties.color = [255, 0, 42, 120];break;
+        case "TMA" : zone.properties.color = [59, 136, 255, 120];break;
+        case "MTMA" : zone.properties.color = [110, 199, 2, 120];break;
+        default: zone.properties.color = [201, 171, 252, 120];
+}
     });
   }
 
