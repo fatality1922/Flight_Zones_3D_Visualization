d8c2c9c08b1637a41986e4c95b1788dced76b667
fatality1922 <fatality1922@gmail.com>
2023-03-30 15:11:15 +0200

    switch way of toggle zones, add burger menu

--- src/App.js
+++ src/App.js
@@ -10,6 +10,10 @@ import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import gif from './gif.gif';
 import HelpIcon from '@mui/icons-material/Help';
+import { slide as Menu } from 'react-burger-menu';
+import './App.css';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
 
 const App = () => {
   const [trueZones, setTrueZones] = useState({
@@ -18,6 +22,60 @@ const App = () => {
   });
 
   const [open, setOpen] = React.useState(true);
+  const [layersVisibility, setLayersVisibility] = useState({
+    TSA: true,
+    TRA: true,
+    TMA: true,
+    TFR: true,
+    RPA: true,
+    RMZ: true,
+    R: true,
+    P: true,
+    NW: true,
+    MTMA: true,
+    MRT: true,
+    MCTR2KM: true,
+    MCTR: true,
+    DRAR: true,
+    DRAP: true,
+    DRAI: true,
+    D: true,
+    CTR6KM: true,
+    CTR1KM: true,
+    CTR: true,
+    ATZ6KM: true,
+    ATZ1KM: true,
+    ATZ: true,
+    AREA: true,
+    ADIZ: true,
+  });
+  const layersColors = {
+    TSA: '#FF002A',
+    TRA: '#C744A5',
+    TMA: '#3B88FF',
+    TFR: '#FF002A',
+    RPA: '#ff7961',
+    RMZ: '#C9ABFC',
+    R: '#FF002A',
+    P: '#f57905',
+    NW: '#41a7fa',
+    MTMA: '#6EC702',
+    MRT: '#0F4F00',
+    MCTR2KM: '#FF0000',
+    MCTR: '#0F4F00',
+    DRAR: '#f52745',
+    DRAP: '#b80620',
+    DRAI: '#41a7fa',
+    D: '#BF1616',
+    CTR6KM: '#B25CCC',
+    CTR1KM: '#FF0000',
+    CTR: '#0F1AD9',
+    ATZ6KM: '#B25CCC',
+    ATZ1KM: '#FF0000',
+    ATZ: '#332315',
+    AREA: '#05727D',
+    ADIZ: '#808069',
+  };
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -27,13 +85,36 @@ const App = () => {
     setOpen(false);
   };
 
+  console.log(process.env.REACT_APP_ZONESS);
+
   return (
     <div>
+      <Menu>
+        <h1>Change visibility of zones</h1>
+        {Object.entries(layersVisibility).map(([key, value]) => (
+          <FormControlLabel
+            key={key}
+            control={
+              <Switch
+                style={{ color: layersColors[key] }} // przypisanie niestandardowego koloru
+                checked={value}
+                onChange={(e) =>
+                  setLayersVisibility((prev) => ({
+                    ...prev,
+                    [key]: e.target.checked,
+                  }))
+                }
+              />
+            }
+            label={key}
+          />
+        ))}
+      </Menu>
+
       <HelpIcon
         onClick={handleClickOpen}
         style={{
           zIndex: 9,
-          position: 'fixed',
           cursor: 'pointer',
           width: '2em',
           height: '2em',
@@ -66,7 +147,11 @@ const App = () => {
         isVerboseMode={false} //If true, the library writes verbose logs to console.
         metaFileDirectory={'.'} //If public assets are hosted somewhere other than root on your server.
       >
-        <MapComponent trueZones={trueZones} setTrueZones={setTrueZones} />
+        <MapComponent
+          trueZones={trueZones}
+          setTrueZones={setTrueZones}
+          layersVisibility={layersVisibility}
+        />
       </CacheBuster>
     </div>
   );
